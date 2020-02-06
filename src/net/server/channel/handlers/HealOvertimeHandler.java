4e09958c218e990a29ab6ec020325f7687f905d6
Jay Friestad <packfanjay@hotmail.com>
2020-02-05 23:03:58 -0600

    Healovertime autoban fix

--- src/net/server/channel/handlers/HealOvertimeHandler.java
+++ src/net/server/channel/handlers/HealOvertimeHandler.java
@@ -61,7 +61,7 @@ public final class HealOvertimeHandler extends AbstractMaplePacketHandler {
         if (healMP != 0 && healMP < 1000) {
             abm.setTimestamp(9, timestamp, 28);
             //if ((abm.getLastSpam(1) + 1500) > timestamp) {
-                AutobanFactory.FAST_MP_HEALING.addPoint(abm, "Fast mp healing");
+              //  AutobanFactory.FAST_MP_HEALING.addPoint(abm, "Fast mp healing");
                 return;     // thanks resinate for noticing mp being gained even after detection
             }
             chr.addMP(healMP);
