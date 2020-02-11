0e3988bb202a02ed3c40d7007f596ed7d76ae324
lightryuzaki <32381002+lightryuzaki@users.noreply.github.com>
2020-02-11 14:38:40 -0800

    FIX: Joas wz patch

--- src/server/gachapon/Ellinia.java
+++ src/server/gachapon/Ellinia.java
@@ -55,12 +55,12 @@ public class Ellinia extends GachaponItems {
 
 	@Override
 	public int[] getUncommonItems() {
-		return new int [] {1082149, 1002391};
+		return new int [] {1082149, 1002391, 1372035, 1372036, 1372037, 1372038};
 	}
 
 	@Override
 	public int[] getRareItems() {
-		return new int [] {};
+		return new int [] {1372039, 1372040, 1372041, 1372042, 1382049, 1382050, 1382051, 1382052};
 	}
 
 }
