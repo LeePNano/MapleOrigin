bce44923c5a93db1bcb45e36aba18bebec7ec3c0
JR <minimucho1@gmail.com>
2020-02-11 23:30:11 -0600

    Lemon: Add glowroot

--- launch.bat
+++ launch.bat
@@ -2,5 +2,5 @@
 @title MapleOrigin Launch Version
 set PATH=C:\Program Files\Java\jdk1.8.0_111\bin;%PATH%
 set CLASSPATH=.;dist\*;cores\*;
-java -Xmx2048m -Dwzpath=wz\ net.server.Server
+java -Xmx2048m -Dwzpath=wz\ -javaagent:glowroot\glowroot.jar net.server.Server
 pause
